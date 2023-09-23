a59f618a55b85fee696c09dbb8e523f053dc7d87
Ahmed Waleed <96238616+ahmedwaleedxx@users.noreply.github.com>
2023-09-23 22:28:27 +0300

    V1_Waleed

---
+++ AM_Hotel/models/Room.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AM_Hotel.Models
+{
+    public abstract class Room
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int roomID { get; set; }
+        public string roomName { get; set;}
+        public string roomFloor { get; set; }
+        public string roomCapacity { get; set; }
+        public string roomAvailability { get; set; }
+
+
+    }
+}
---
+++ AM_Hotel/models/SingleRoom.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AM_Hotel.Models
+{
+    public class SingleRoom : Room
+    {
+        public const int roomPrice = 80;
+    }
+}
